f6cec159f826ffa2687517ffd80635461e4ee5e7
Faig-Abaszada <faik.abaszada@gmail.com>
2021-05-22 17:56:57 +0400

    interface data tipleri (string , number ve ya..)

--- src/app/models/card.ts
+++ src/app/models/card.ts
@@ -1,2 +1,9 @@
 export interface Card {
+  id: number;
+  name?: string;
+  title: string;
+  phone: string;
+  email?: string;
+  address?: string;
+  // burada "?" isareleri hemin datanin onemsiz oldugunu bildirir lakin digerleri cox onemlidir
 }
